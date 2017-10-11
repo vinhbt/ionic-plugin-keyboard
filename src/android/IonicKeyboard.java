9da969ad7531b2d2bec550713c20433b5ab0a9ac
VinhBT <vinhbt@sandinh.net>
2017-10-11 11:32:59 +0700

    Avoid Android crashes caused by onDestroy NullpointerExceptions

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -128,7 +128,10 @@ public class IonicKeyboard extends CordovaPlugin {
 
     @Override
     public void onDestroy() {
-        rootView.getViewTreeObserver().removeOnGlobalLayoutListener(list);
+        if (rootView != null && rootView.getViewTreeObserver() != null){
+            rootView.getViewTreeObserver().removeOnGlobalLayoutListener(list);
+        }
+        super.onDestroy();
     }
 
 }
